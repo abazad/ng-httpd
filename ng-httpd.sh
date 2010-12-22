cdca4b693bb0b9b7cf71a6076ab6829d567f59fa
Alexey Rakeev <alexey@rakeev.name>
2010-12-22 19:26:23 +0400

    Implemented enable/disable commands, queueing

--- ng-httpd.sh
+++ ng-httpd.sh
@@ -3,8 +3,17 @@ cmd=$1
 user=$2
 domain=$3
 
+APCONFDIR=/etc/httpd/conf
 NGCONFDIR=/usr/local/nginx/conf
-DAUSERDIR=/usr/local/directadmin/data/users
+DAROOTDIR=/usr/local/directadmin
+DAUSERDIR=$DAROOTDIR/data/users
+DATEMPDIR=$DAROOTDIR/data/templates
+DAQUEUE=$DAROOTDIR/data/task.queue
+TEMPLATES="virtual_host virtual_host_sub virtual_host_secure virtual_host_secure_sub \
+	virtual_host2 virtual_host2_sub virtual_host2_secure virtual_host2_secure_sub \
+	ips_virtual_host redirect_virtual_host"
+EXTPORT=80
+INTPORT=8888
 
 add() {
 	if [[ -z $user || -z $domain ]]; then
@@ -99,7 +108,7 @@ add() {
 	done
 }
 
-del() {
+delete() {
 	if [ -z $user ]; then
 		usage
 		exit 1
@@ -129,16 +138,118 @@ build() {
 	done
 }
 
+updateips() {
+	sed -i "s/:$EXTPORT/:$INTPORT/g" $APCONFDIR/ips.conf
+	ips=$(cat $DAROOTDIR/data/admin/ip.list | awk 'BEGIN {FS = "[ \t:]+"; ORS=" "} {print $1}')
+	cat > $APCONFDIR/extra/httpd-rpaf.conf <<EOF
+LoadModule rpaf_module /usr/lib/apache/mod_rpaf-2.0.so
+RPAFenable On
+RPAFsethostname On
+RPAFproxy_ips $ips
+RPAFheader X-Real-IP
+EOF
+}
+
+enable() {
+	cd $DATEMPDIR
+	mkdir -p custom
+
+	for tpl in $TEMPLATES
+	do
+		if [ ! -f custom/$tpl.conf ]; then
+			cp $tpl.conf custom/
+		fi
+		sed -i "s/:$EXTPORT/:$INTPORT/g" custom/$tpl.conf
+	done
+
+	echo "action=rewrite&value=ips" >> $DAQUEUE
+	echo "action=rewrite&value=httpd" >> $DAQUEUE
+	$DAROOTDIR/dataskq
+	if [ $? != 0 ]; then
+		echo "Apache config failed"
+		exit 1
+	fi
+
+	updateips
+	sed -i -e "s/$EXTPORT/$INTPORT/g" -e "/httpd-ssl/ s/^\s*#*\s*/#/" \
+		-e "/httpd-rpaf/ s/^\s*#*\s*//" $APCONFDIR/httpd.conf
+	echo "action=httpd&value=restart" >> $DAQUEUE
+	$DAROOTDIR/dataskq
+	if [ $? != 0 ]; then
+		echo "Apache restart failed"
+		exit 1
+	fi
+
+	sed -i "s/nginx=OFF/nginx=ON/" $DAROOTDIR/data/admin/services.status
+	/etc/init.d/nginx start > /dev/null
+	if [ $? != 0 ]; then
+		echo "Nginx start failed"
+		exit 1
+	fi
+}
+
+disable() {
+	sed -i "s/nginx=ON/nginx=OFF/" $DAROOTDIR/data/admin/services.status
+	/etc/init.d/nginx stop > /dev/null
+	if [ $? != 0 ]; then
+		echo "Nginx stop failed"
+		exit 1
+	fi
+
+	if [ -d $DATEMPDIR/custom ]; then
+		cd $DATEMPDIR/custom
+
+		for tpl in $TEMPLATES
+		do
+			if [ -f $tpl.conf ]; then
+				sed -i "s/:$INTPORT/:$EXTPORT/g" $tpl.conf
+			fi
+		done
+	fi
+
+	sed -i -e "s/$INTPORT/$EXTPORT/g" -e "/httpd-ssl/ s/^\s*#*\s*//" \
+		-e "/httpd-rpaf/ s/^\s*#*\s*/#/" $APCONFDIR/httpd.conf
+	echo "action=rewrite&value=ips" >> $DAQUEUE
+	echo "action=rewrite&value=httpd" >> $DAQUEUE
+	echo "action=httpd&value=restart" >> $DAQUEUE
+	$DAROOTDIR/dataskq
+	if [ $? != 0 ]; then
+		echo "Apache restart failed"
+		exit 1
+	fi
+}
+
+queue() {
+	cd $(dirname $0)
+	if [ ! -f queue ]; then
+		touch queue
+		chgrp diradmin queue
+		chmod 664 queue
+		exit 0
+	fi
+
+	while read line
+	do
+		$($0 $line)
+	done < queue
+
+	echo -n "" > queue
+}
+
 usage() {
 	echo "Usage:"
-	echo "$0 build"
 	echo "$0 add user domain"
-	echo "$0 del user [domain]"
+	echo "$0 delete user [domain]"
+	echo "$0 (build|enable|disable|updateips|queue)"
 }
 
 case $cmd in
-	"add"	) add;;
-	"del"	) del;;
-	"build"	) build;;
-	*		) usage;;
+	"add"		) add;;
+	"delete"	) delete;;
+	"build"		) build;;
+	"enable"	) enable;;
+	"disable"	) disable;;
+	"updateips"	) updateips;;
+	"queue"		) queue;;
+	*			) usage;;
 esac
