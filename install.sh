f2096fbb9a1001c808299a59a99360b918caed45
Alexey Rakeev <alexey@rakeev.name>
2012-01-25 12:42:53 +0400

    Added prefix var

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 CURDIR=$(cd $(dirname "$0"); pwd)
+PREFIX=/usr/local/nginx
 NGLATEST="nginx-1.0.9"
 OK='\033[01;32m'
 DO='\033[01;35m'
@@ -26,7 +27,7 @@ else
 fi
 
 echo -ne "Checking for Nginx installed "
-if [ -f /usr/local/nginx/sbin/nginx ]; then
+if [ -f $PREFIX/sbin/nginx ]; then
 	echo -e "[$OK OK $RS]"
 else
 	echo -e "[$DO NO $RS]"
@@ -104,11 +105,11 @@ echo -e "[$DO DONE $RS]"
 echo -ne "Copying new files "
 cd $CURDIR
 cp -f httpd-ng.conf /etc/httpd/conf/extra/
-cp -f ng-httpd.sh /usr/local/nginx/sbin/
-chmod 755 /usr/local/nginx/sbin/ng-httpd.sh
-cp -f conf/* /usr/local/nginx/conf/
-mkdir -p /usr/local/nginx/conf/extra
-/usr/local/nginx/sbin/ng-httpd.sh queue
+cp -f ng-httpd.sh $PREFIX/sbin/
+chmod 755 $PREFIX/sbin/ng-httpd.sh
+cp -f conf/* $PREFIX/conf/
+mkdir -p $PREFIX/conf/extra
+$PREFIX/sbin/ng-httpd.sh queue
 cp -f logrotate /etc/logrotate.d/nginx
 mkdir -p /usr/local/directadmin/plugins/ng-httpd
 cp -Rf plugin/* /usr/local/directadmin/plugins/ng-httpd/
@@ -116,7 +117,7 @@ chown -R diradmin:diradmin /usr/local/directadmin/plugins/ng-httpd
 chmod 755 /usr/local/directadmin/plugins/ng-httpd/scripts/*.sh
 echo -e "[$DO DONE $RS]"
 
-(crontab -l | sed /ng-httpd/d; echo "* * * * * /usr/local/nginx/sbin/ng-httpd.sh queue") | crontab -
+(crontab -l | sed /ng-httpd/d; echo "* * * * * $PREFIX/sbin/ng-httpd.sh queue") | crontab -
 E=$?
 echo -ne "Installing crontab "
 if [ $E != 0 ]; then
@@ -125,7 +126,7 @@ if [ $E != 0 ]; then
 fi
 echo -e "[$DO DONE $RS]"
 
-/usr/local/nginx/sbin/ng-httpd.sh enable
+$PREFIX/sbin/ng-httpd.sh enable
 E=$?
 echo -ne "Enabling nginx frontend "
 sed -i /nginx/d /usr/local/directadmin/data/admin/services.status
